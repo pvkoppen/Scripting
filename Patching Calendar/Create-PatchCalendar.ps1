df4884196e814d89314e26216b797b59d67643f4
pvkoppen <59666031+pvkoppen@users.noreply.github.com>
2020-03-01 21:04:40 +1300

    update to alter recurrence

--- Patching Calendar/Create-PatchCalendar.ps1
+++ Patching Calendar/Create-PatchCalendar.ps1
@@ -28,12 +28,12 @@ ORGANIZER;CN="John Smith":mailto:jsmith@example.com
         Write-Host "CREATED:$(WriteDateTime $script:dtStamp)"
         Write-Host "DTSTAMP:$(WriteDateTime $script:dtStamp)"
         Write-Host "LAST-MODIFIED:$(WriteDateTime $script:dtStamp)"
-        Write-Host "RRULE:FREQ=DAILY;UNTIL=$(WriteDateTime ($script:dtStamp).AddDays($occurances))"
-        Write-Host "SUMMARY:$($Subject)"
+        Write-Host "RRULE:FREQ=DAILY;UNTIL=$(WriteDateTime ($script:dtStamp).AddDays($occurances-1))"
+        Write-Host "SUMMARY:Windows Patching - $($Subject)"
         Write-Host "TRANSP:OPAQUE"
-        Write-Host "UID:MARCH-2020"
+        Write-Host "UID:WINDOWS-PATCHING-2020-MARCH"
         Write-Host "LOCATION:Methanex NZ"
-        Write-Host "DESCRIPTION:$($Subject)"
+        Write-Host "DESCRIPTION:Windows Patching - $($Subject)"
         Write-Host "END:VEVENT"
     }
 <#
@@ -61,17 +61,17 @@ END:VALARM
         Write-Host "CREATED:$(WriteDateTime $script:dtStamp)"
         Write-Host "DTSTAMP:$(WriteDateTime $script:dtStamp)"
         Write-Host "LAST-MODIFIED:$(WriteDateTime $script:dtStamp)"
-        Write-Host "RECURRENCE-ID:$(WriteDateTime ($script:dtStamp).AddDays($Instance))"
+        Write-Host "RECURRENCE-ID:$(WriteDateTime ($script:dtStamp).AddDays($Instance-1))"
         Write-Host "DTEND:$(WriteDateTime $Finish)"
         Write-Host "DTSTART:$(WriteDateTime $Start)"
-        Write-Host "SUMMARY:$($Subject)"
+        Write-Host "SUMMARY:Windows Patching - $($Subject)"
         #Write-Host "TRANSP:OPAQUE"
-        Write-Host "UID:MARCH-2020"
+        Write-Host "UID:WINDOWS-PATCHING-2020-MARCH"
         #Write-Host "LOCATION:Methanex NZ"
-        Write-Host "DESCRIPTION:$($Subject)"
+        Write-Host "DESCRIPTION:Windows Patching - $($Subject)"
         Write-Host "BEGIN:VALARM"
         Write-Host "TRIGGER:-PT30M"
-        #Write-Host "DESCRIPTION:$($Subject)"
+        #Write-Host "DESCRIPTION:Windows Patching - $($Subject)"
         Write-Host "ACTION:DISPLAY"
         Write-Host "END:VALARM"
         Write-Host "END:VEVENT"
@@ -85,40 +85,39 @@ END:VALARM
     } else {
         $ProcessDate = Get-Date -Date (Get-Date -format "yyyy-MM-05")
     }
-    $SecondTuesday = $ProcessDate.AddDays(2-$ProcessDate.DayOfWeek)
+    $PatchTuesday  = $ProcessDate.AddDays(9-$ProcessDate.DayOfWeek)
     $ProcessDate   = $ProcessDate.AddDays(-4)
     Write-Host "Check for Month: '$($ProcessDate.ToLongDateString())'"
-    #Write-Host "Patch Tuesday: '$($SecondTuesday.ToLongDateString())'"
-#
+    Write-Host "Patch Tuesday: '$(($PatchTuesday).ToLongDateString())'"
     #Friday after second Tuesday
-    $TestChangeLogStart     = ($SecondTuesday).AddDays(03).AddHours(09) 
+    $TestChangeLogStart     = ($PatchTuesday).AddDays(03).AddHours(09) 
     $TestChangeLogEnd       = ($TestChangeLogStart).AddHours(00).AddMinutes(30)
     #Friday-Week after second Tuesday
-    $ProdChangeLogStart     = ($SecondTuesday).AddDays(10).AddHours(09) 
+    $ProdChangeLogStart     = ($PatchTuesday).AddDays(10).AddHours(09) 
     $ProdChangeLogEnd       = ($ProdChangeLogStart).AddHours(00).AddMinutes(30)
     #Wednesday-Week after second Tuesday
-    $TestCheckStart         = ($SecondTuesday).AddDays(08).AddHours(09) 
+    $TestCheckStart         = ($PatchTuesday).AddDays(08).AddHours(09) 
     $TestCheckEnd           = ($TestCheckStart).AddHours(00).AddMinutes(30)
     #Thursday-Week after second Tuesday
-    $TestPatchStart         = ($SecondTuesday).AddDays(09).AddHours(15) 
+    $TestPatchStart         = ($PatchTuesday).AddDays(09).AddHours(15) 
     $TestPatchEnd           = ($TestPatchStart).AddHours(03).AddMinutes(00)
     #Monday-Week after second Tuesday
-    $ProdFirstNotifyStart   = ($SecondTuesday).AddDays(13).AddHours(09) 
+    $ProdFirstNotifyStart   = ($PatchTuesday).AddDays(13).AddHours(09) 
     $ProdFirstNotifyEnd     = ($ProdFirstNotifyStart).AddHours(00).AddMinutes(30)
     #Wednesday-2Week after second Tuesday
-    $ProdSecondNotifyStart  = ($SecondTuesday).AddDays(15).AddHours(09) 
+    $ProdSecondNotifyStart  = ($PatchTuesday).AddDays(15).AddHours(09) 
     $ProdSecondNotifyEnd    = ($ProdSecondNotifyStart).AddHours(00).AddMinutes(30)
     #Thursday-2Week after second Tuesday
-    $ProdStanddownStart     = ($SecondTuesday).AddDays(16).AddHours(08) 
+    $ProdStanddownStart     = ($PatchTuesday).AddDays(16).AddHours(08) 
     $ProdStanddownEnd       = ($ProdStanddownStart).AddHours(04).AddMinutes(00)
     #Thursday-2Week after second Tuesday
-    $ProdCheckStart         = ($SecondTuesday).AddDays(16).AddHours(16) 
+    $ProdCheckStart         = ($PatchTuesday).AddDays(16).AddHours(16) 
     $ProdCheckEnd           = ($ProdCheckStart).AddHours(00).AddMinutes(30)
     #Thursday-2Week after second Tuesday
-    $ProdFirstPatchStart    = ($SecondTuesday).AddDays(16).AddHours(19) 
+    $ProdFirstPatchStart    = ($PatchTuesday).AddDays(16).AddHours(19) 
     $ProdFirstPatchEnd      = ($ProdFirstPatchStart).AddHours(05).AddMinutes(-1)
     #Friday-2Week after second Tuesday
-    $ProdSecondPatchStart   = ($SecondTuesday).AddDays(17).AddHours(09) 
+    $ProdSecondPatchStart   = ($PatchTuesday).AddDays(17).AddHours(09) 
     $ProdSecondPatchEnd     = ($ProdSecondPatchStart).AddHours(03).AddMinutes(00)
     #First workday of next month
     $ScheduleNextMonthStart = ($ProcessDate).AddMonths(1).AddHours(09)
@@ -128,7 +127,7 @@ END:VALARM
     }
     $ScheduleNextMonthEnd   = ($ScheduleNextMonthStart).AddHours(00).AddMinutes(30)
 <#
-    Write-Host "Patch Tuesday: '$($SecondTuesday.ToLongDateString())'."
+    Write-Host "Patch Tuesday: '$($PatchTuesday.ToLongDateString())'."
     Write-Host "Test change logged: '$($TestChangeLogStart.ToString('f'))', finish at '$($TestChangeLogEnd.ToShortTimeString())'."
     Write-Host "Test check: '$($TestCheckStart.ToString('f'))', finish at '$($TestCheckEnd.ToShortTimeString())'."
     Write-Host "Test patch: '$($TestPatchStart.ToString('f'))', finish at '$($TestPatchEnd.ToShortTimeString())'."
@@ -141,8 +140,8 @@ END:VALARM
     Write-Host "Prod second patch: '$($ProdSecondPatchStart.ToString('f'))', finish at '$($ProdSecondPatchEnd.ToShortTimeString())'."
     Write-Host "Schedule next month: '$($ScheduleNextMonthStart.ToString('f'))', finish at '$($ScheduleNextMonthEnd.ToShortTimeString())'."
 #>
-StartCalendar -Occurances 12 -Subject "Windows Patching $(($ProcessDate).ToString("yyyy-MM (MMM)"))"
-AddCalendarDate -Instance 01 -Start ($SecondTuesday).AddHours(09) -Finish ($SecondTuesday).AddHours(09) -Subject "Patch Tuesday"
+StartCalendar -Occurances 12 -Subject "$(($ProcessDate).ToString("yyyy-MM (MMM)"))"
+AddCalendarDate -Instance 01 -Start ($PatchTuesday).AddHours(09) -Finish ($PatchTuesday).AddHours(09) -Subject "Patch Tuesday"
 AddCalendarDate -Instance 02 -Start $TestChangeLogStart -Finish $TestChangeLogEnd -Subject "Test change logged"
 AddCalendarDate -Instance 03 -Start $TestCheckStart -Finish $TestCheckEnd -Subject "Test check"
 AddCalendarDate -Instance 04 -Start $TestPatchStart -Finish $TestPatchEnd -Subject "Test patch"
