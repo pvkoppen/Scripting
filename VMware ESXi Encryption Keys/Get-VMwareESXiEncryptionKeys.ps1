65966a67af5cba503d1817bb80d863dd5437632a
pvkoppen <pvkoppen@gmail.com>
2023-06-27 21:58:08 +1200

    certificates

---
+++ VMware ESXi Encryption Keys/Get-VMwareESXiEncryptionKeys.ps1
@@ -0,0 +1,41 @@
+Connect-VIServer -Server [name]
+
+#
+$VMHosts = Get-VMHost | Sort-Object
+$VMHostKeys = @()
+foreach ($VMHost in $VMHosts) {
+    $esxcli = Get-EsxCli -VMHost $VMHost -V2
+    try {
+        $encryption = $esxcli.system.settings.encryption.get.Invoke()
+        if ($encryption.Mode -eq "TPM")
+        {
+            $key = $esxcli.system.settings.encryption.recovery.list.Invoke()
+            $hostKey = [pscustomobject]@{
+                Host = $VMHost.Name
+                EncryptionMode = $encryption.Mode
+                RequireExecutablesOnlyFromInstalledVIBs = $encryption.RequireExecutablesOnlyFromInstalledVIBs
+                RequireSecureBoot = $encryption.RequireSecureBoot
+                RecoveryID = $key.RecoveryID
+                RecoveryKey = $key.Key
+            }
+            $VMHostKeys += $hostKey
+        }
+        else
+        {
+            $hostKey = [pscustomobject]@{
+                Host = $VMHost.Name
+                EncryptionMode = $encryption.Mode
+                RequireExecutablesOnlyFromInstalledVIBs = $encryption.RequireExecutablesOnlyFromInstalledVIBs
+                RequireSecureBoot = $encryption.RequireSecureBoot
+                RecoveryID = $null
+                RecoveryKey = $null
+            }
+            $VMHostKeys += $hostKey
+        }
+    }
+    catch {
+        $VMHost.Name + $_
+    }
+}
+$VMHostKeys
+Disconnect-VIServer
