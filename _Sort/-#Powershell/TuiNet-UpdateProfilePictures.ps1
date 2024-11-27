918a0ea9e9117c4f5f4eaedf89b4d440bc534276
pvkoppen <59666031+pvkoppen@users.noreply.github.com>
2024-11-27 21:47:20 +1300

    cleanup duplicates

--- _Sort/-#Powershell/TuiNet-UpdateProfilePictures.ps1
+++
@@ -1,2 +0,0 @@
-Add-PsSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
-Update-SPProfilePhotoStore -CreateThumbnailsForImportedPhotos 1 –MySiteHostLocation http://mysites.tuiora.co.nz
